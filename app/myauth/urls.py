1287f3836a8c13e14bab20c68f51f5f7989e2ba5
Isaac <r.sato7690@me.com>
2017-03-13 15:45:27 +0900

    Add auth

---
+++ app/myauth/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib.auth.views import logout
+
+from . import views
+
+app_name = 'myauth'
+urlpatterns = [
+    url(r'^login$', views.login, name='login'),
+    url(r'^logout$', logout, name='logout'),
+]
