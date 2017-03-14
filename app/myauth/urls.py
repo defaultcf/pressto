c5a6260924852f9d9218e57060ae13baea040383
Isaac <r.sato7690@me.com>
2017-03-14 16:01:12 +0900

    Add profile model

--- app/myauth/urls.py
+++ app/myauth/urls.py
@@ -7,4 +7,5 @@ app_name = 'myauth'
 urlpatterns = [
     url(r'^login$', views.login, name='login'),
     url(r'^logout$', logout, name='logout'),
+    url(r'^setting$', views.setting, name='setting'),
 ]
--- app/myauth/views.py
+++ app/myauth/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request, 'myauth/login.html')
+
+def setting(request):
+    if request.POST:
+        user = User.objects.get(pk=request.user.id)
+        user.profile.nickname = request.POST['nickname']
+        user.profile.bio = request.POST['bio']
+        user.save()
+
+    return render(request, 'myauth/setting.html')
